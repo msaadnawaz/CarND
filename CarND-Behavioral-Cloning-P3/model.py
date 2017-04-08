bfbd52f34e773dc917f1184e980bf4c10cd6ddfe
Muhammad Saad Nawaz <saadnawaz27@yahoo.com>
2017-04-08 17:12:49 +0200

    P3 model development and training

--- CarND-Behavioral-Cloning-P3/model.py
+++ CarND-Behavioral-Cloning-P3/model.py
@@ -9,7 +9,7 @@ import cv2
 
 import numpy as np
 import tensorflow as tf
-from keras.layers import Lambda, Dense, Convolution2D, Flatten, MaxPooling2D, Activation
+from keras.layers import Lambda, Dense, Conv2D, Flatten, MaxPooling2D, Activation, Cropping2D, Dropout
 from keras.models import Sequential
 
 lines=[]
@@ -33,8 +33,11 @@ for line in lines:
     source_path = line[2]
     image_right=cv2.imread(source_path)
     
+    image_center_flipped=np.fliplr(image_center)
+    
     #add only center images to dataset for training
     images.append(image_center)
+    images.append(image_center_flipped)
     #add all 3 camera images to dataset for training
     #images.extend(image_center, image_left, image_right)
     
@@ -44,6 +47,7 @@ for line in lines:
     
     #add only center image angle to dataset for training
     angles.append(angle_center)
+    angles.append(angle_center*-1)
     #add all 3 angles to dataset for training
     #angles.extend(angle_center, angle_left, angle_right)
 
@@ -52,17 +56,25 @@ y_train=np.array(angles)
 
 #Here is the definition of neural network
 model=Sequential()
-#model.add(Convolution2D())
-#model.add(Maxpooling2D())
-#model.add(Convolution2D())
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x:x/128-1, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Conv2D(6,(5,5), activation='relu'))
+model.add(MaxPooling2D(2,2))
+model.add(Conv2D(12,(5,5), activation='relu'))
+model.add(MaxPooling2D(2,2))
+model.add(Conv2D(24,(5,5), activation='relu'))
+model.add(MaxPooling2D(2,2))
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(240))
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 #model.add(Activation('relu'))
 #model.add(Dense())
-#model.add(Activation('softmax'))
 
 model.compile(loss='mse', optimizer='adam')
 
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=100)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10)
 
 model.save('model.h5')
