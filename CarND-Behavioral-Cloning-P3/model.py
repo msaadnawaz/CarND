24857e68d019571c487a125b7e0f8b96970ef73f
Muhammad Saad Nawaz <saadnawaz27@yahoo.com>
2017-04-10 00:05:51 +0200

    model update with customized architecture and fit_generator

--- CarND-Behavioral-Cloning-P3/model.py
+++ CarND-Behavioral-Cloning-P3/model.py
@@ -6,11 +6,24 @@ Created on Thu Apr  6 19:19:09 2017
 """
 import csv
 import cv2
-
+import math
 import numpy as np
 import tensorflow as tf
 from keras.layers import Lambda, Dense, Conv2D, Flatten, MaxPooling2D, Activation, Cropping2D, Dropout
 from keras.models import Sequential
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+def generator(X_data, y_data, batch_size=128):
+    while 1:
+        for offset in range(0, len(X_data), batch_size):
+            end=offset+batch_size
+            X_batch=X_data[offset:end]
+            y_batch=y_data[offset:end]
+        yield shuffle(X_batch, y_batch)
 
 lines=[]
 
@@ -51,8 +64,16 @@ for line in lines:
     #add all 3 angles to dataset for training
     #angles.extend(angle_center, angle_left, angle_right)
 
-X_train=np.array(images)
-y_train=np.array(angles)
+X_data=np.array(images)
+y_data=np.array(angles)
+
+X_data, y_data=shuffle(X_data, y_data)
+X_train, X_valid, y_train, y_valid = train_test_split(X_data, y_data, test_size=0.2)
+
+batch_size=128
+
+train_generator=generator(X_train, y_train, batch_size)
+valid_generator=generator(X_valid, y_valid, batch_size)
 
 #Here is the definition of neural network
 model=Sequential()
@@ -67,14 +88,32 @@ model.add(MaxPooling2D(2,2))
 model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(240))
+model.add(Activation('relu'))
 model.add(Dense(120))
+model.add(Activation('relu'))
 model.add(Dense(84))
+model.add(Activation('relu'))
 model.add(Dense(1))
-#model.add(Activation('relu'))
-#model.add(Dense())
+
 
 model.compile(loss='mse', optimizer='adam')
 
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10)
+#model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10)
+
+history_object = model.fit_generator(train_generator, steps_per_epoch =
+    math.ceil(len(X_train)/batch_size), validation_data = 
+    valid_generator,
+    nb_val_samples = math.ceil(len(X_valid)/batch_size), 
+    nb_epoch=10, verbose=1)
 
 model.save('model.h5')
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
+plt.savefig('graph.jpg')
