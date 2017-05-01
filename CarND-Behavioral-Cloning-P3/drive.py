5566642dc51903c356989df26304fd444cef6aa2
Muhammad Saad Nawaz <saadnawaz27@yahoo.com>
2017-05-02 00:49:55 +0200

    P3 drive.py tweak

--- CarND-Behavioral-Cloning-P3/drive.py
+++ CarND-Behavioral-Cloning-P3/drive.py
@@ -62,9 +62,9 @@ def telemetry(sid, data):
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
-
+		
         throttle = controller.update(float(speed))
-
+        
         print(steering_angle, throttle)
         send_control(steering_angle, throttle)
 
